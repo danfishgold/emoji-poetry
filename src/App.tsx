7cef16525eb2f297f5858401abea62b35d0e3197
Dan Fishgold <d.fishgold@gmail.com>
2021-02-28 20:40:56 +0200

    Tidy up presets and add another one

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ import { random } from './util'
 const commonMeter = 'x/x/x/x/\nx/x/x/ (A)\nx/x/x/x/\nx/x/x/ (A)'
 const tweet =
   '/xx/x (A)\n/xx/ (B)\n/xx/x (A)\n/xx/ (B)\n\n/xx/x\n/xx/ (C)\n/xx/xx/xx/ (C)\n\n/xx/xx/xx/ (D)\n/xx/xx/xx/ (D)\n\n/xx/xx/xx/ (E)\n/xx\n/xx\n/xx\n/ (E)'
-const tips = `tip #1: you can use words and emoji together /xx/x even in the same line. be careful of the letter x\n\ntip #2: you can also have internal rhymes in the same line:\n\n/(A)/x(B)/xx(C)/(A)/x(B)/xx(C)\n/xx/xx/xx/(D)\n\n/(E)/x(F)/xx(G)/(E)/x(F)/xx(G)\n/xx/x\nx/x\nx/(D)\n\ntip #3: short rhyming sequences tend to be repetitive, so you have to randomize them a little to get something interesting`
+const tips = `tip #1: you can use words and emoji together /xx/x even in the same line. be careful of the letter x\n\ntip #2: you can also have internal rhymes in the same line:\n\n/(A)/x(B)/xx(C)/(A)/x(B)/xx(C)\n/xx/xx/xx/(D)\n\n/(E)/x(F)/xx(G)/x(F)/(E)/xx(G)\n/xx/x\nx/x\nx/(D)\n\ntip #3: short rhyming sequences tend to be repetitive, so you have to randomize them a little to get something interesting`
+const callMeMaybe =
+  '(to the tune of call me maybe)\n\n/ /x/x\nx/x/x(A)\nx/x/x\nx/x/x(A)'
 
 function App() {
   const [
@@ -43,6 +45,9 @@ function App() {
           </button>
           <button onClick={() => setTemplate(commonMeter)}>common meter</button>
           <button onClick={() => setTemplate(tips)}>tips</button>
+          <button onClick={() => setTemplate(callMeMaybe)}>
+            call me maybe
+          </button>
         </div>
         <p>or build one yourself:</p>
         <ul>
