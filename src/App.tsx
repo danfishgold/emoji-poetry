92d90c9a52f806352cd361ba9d3a141f280e31d5
Dan Fishgold <d.fishgold@gmail.com>
2021-02-28 19:32:14 +0200

    Add an example poem with internal rhyming

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { random } from './util'
 const commonMeter = 'x/x/x/x/\nx/x/x/ (A)\nx/x/x/x/\nx/x/x/ (A)'
 const tweet =
   '/xx/x (A)\n/xx/ (B)\n/xx/x (A)\n/xx/ (B)\n\n/xx/x\n/xx/ (C)\n/xx/xx/xx/ (C)\n\n/xx/xx/xx/ (D)\n/xx/xx/xx/ (D)\n\n/xx/xx/xx/ (E)\n/xx\n/xx\n/xx\n/ (E)'
-const tips = `tip #1: you can use words and emoji together /xx/x even in the same line. be careful of the letter x\n\ntip #2: you can also have internal rhymes in the same line: /xx/(A)x/xx/(A)`
+const tips = `tip #1: you can use words and emoji together /xx/x even in the same line. be careful of the letter x\n\ntip #2: you can also have internal rhymes in the same line:\n\n/(A)/x(B)/xx(C)/(A)/x(B)/xx(C)\n/xx/xx/xx/(D)\n\n/(E)/x(F)/xx(G)/(E)/x(F)/xx(G)\n/xx/x\nx/x\nx/(D)\n\ntip #3: short rhyming sequences tend to be repetitive, so you have to randomize them a little to get something interesting`
 
 function App() {
   const [
