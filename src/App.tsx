e7d31399dc64bf01d512b79e005fe76f477c43d7
Dan Fishgold <d.fishgold@gmail.com>
2021-02-28 18:52:26 +0200

    Change the inline rhyming example to be more flexible

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const limerick =
 const commonMeter = 'x/x/x/x/\nx/x/x/ (A)\nx/x/x/x/\nx/x/x/ (A)'
 const tweet =
   '/xx/x (A)\n/xx/ (B)\n/xx/x (A)\n/xx/ (B)\n\n/xx/x\n/xx/ (C)\n/xx/xx/xx/ (C)\n\n/xx/xx/xx/ (D)\n/xx/xx/xx/ (D)\n\n/xx/xx/xx/ (E)\n/xx\n/xx\n/xx\n/ (E)'
-const tips = `tip #1: you can use words and emoji together /xx/x even in the same line. be careful of the letter x\n\ntip #2: you can also have internal rhymes in the same line: /xx/(A)xx/xx/(A)`
+const tips = `tip #1: you can use words and emoji together /xx/x even in the same line. be careful of the letter x\n\ntip #2: you can also have internal rhymes in the same line: /xx/(A)x/xx/(A)`
 
 function App() {
   const [
